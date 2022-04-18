443b661e88c26d3c2c3e54c98724f69f5fc29776
Dian Tri Budianto <dyanzzz@gmail.com>
2022-04-18 14:54:07 +0700

    Remove restricted character in field title implement jwt to pat /threads add validation to ADD_THREAD in DomainErrorTranslator

--- src/Domains/threads/entities/_test/AddThread.test.js
+++ src/Domains/threads/entities/_test/AddThread.test.js
@@ -35,18 +35,6 @@ describe('a AddThread entities', () => {
     expect(() => new AddThread(payload)).toThrowError('ADD_THREAD.TITLE_LIMIT_CHAR');
   });
 
-  it('should throw error when title contains restricted character', () => {
-    // Arrange
-    const payload = {
-      title: 'dico ding',
-      body: 'dicoding',
-      owner: 'user-123'
-    };
-
-    // Action and Assert
-    expect(() => new AddThread(payload)).toThrowError('ADD_THREAD.TITLE_CONTAIN_RESTRICTED_CHARACTER');
-  });
-
   it('should create AddThread object correctly', () => {
     // Arrange
     const payload = {
