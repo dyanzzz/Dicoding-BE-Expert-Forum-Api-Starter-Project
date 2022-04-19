a527b7aacc774cc12c1feed1460b799f93fe3e4a
Dian Tri Budianto <dyanzzz@gmail.com>
2022-04-19 11:24:06 +0700

    update migrate table to add field date remove migrate table foreignkey threads

--- migrations/1650234408531_create-table-threads.js
+++ migrations/1650234408531_create-table-threads.js
@@ -13,11 +13,17 @@ exports.up = (pgm) => {
       type: 'TEXT',
       notNull: true,
     },
+    date: {
+      type: 'VARCHAR(30)',
+      notNull: true,
+    },
     owner: {
       type: 'TEXT',
       notNull: true,
     },
   });
+
+  pgm.addConstraint('threads', 'fk_threads.owner_user.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
--- migrations/1650234705770_foreignkey-threads.js
+++
@@ -1,8 +0,0 @@
-exports.up = (pgm) => {
-  // memberikan constraint foreignkey
-  pgm.addConstraint('threads', 'fk_threads.owner_user.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
-};
-
-exports.down = (pgm) => {
-  pgm.dropConstraint('threads', 'fk_threads.owner_user.id');
-};
